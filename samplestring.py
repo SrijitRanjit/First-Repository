fc65e56ecddfe89b6c0b498fd449705701745ddd
SrijitRanjit <62109426+SrijitRanjit@users.noreply.github.com>
2020-03-12 23:30:44 +0800

    Create samplestring.py

---
+++ samplestring.py
@@ -0,0 +1,9 @@
+#
+# Student Name: Srijit Ranjit
+# Student ID: 20090298
+#
+# practest1a.py : Read in a string and print it
+#
+
+instring = input('Enter a string')
+print('\nThe string is:', instring)
